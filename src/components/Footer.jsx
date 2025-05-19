84ed52cf71f80385e758c053dac0b97e011a6803
Mathusan Aiyadurai Iyer <22655198+Mathu145@users.noreply.github.com>
2025-05-19 15:10:03 +0200

    add footer informations

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       {" "}
       <p className="text-sm text-muted-foreground">
         {" "}
-        &copy; {new Date().getFullYear()} Mathusan Aiyadurai Iyer. All rights reserved. 
+        &copy; {new Date().getFullYear()} Mathusan Sharma Aiyadurai Iyer. All rights reserved. 
       </p>
       <a
         href="#hero"
