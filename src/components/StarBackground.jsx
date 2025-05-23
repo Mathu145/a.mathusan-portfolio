3958eda6bf47c0a8fbf7474b72d144c5208018bf
Mathusan Aiyadurai Iyer <22655198+Mathu145@users.noreply.github.com>
2025-05-23 14:03:09 +0200

    style adjustment

--- src/components/StarBackground.jsx
+++ src/components/StarBackground.jsx
@@ -133,13 +133,13 @@ export const StarBackground = () => {
           key={meteor.id}
           className="meteor animate-meteor"
           style={{
-            width: `${meteor.size * 50}px`,
-            height: `${meteor.size * 2}px`,
             left: `${meteor.x}%`,
             top: `${meteor.y}%`,
             animationDelay: `${meteor.delay}s`,
             animationDuration: `${meteor.animationDuration}s`,
             '--meteor-color': meteor.color,
+            '--meteor-width': `${meteor.size * 50}px`,
+            '--meteor-height': `${meteor.size * 2}px`,
           }}
         />
       ))}
